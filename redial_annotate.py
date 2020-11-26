ffcdb257cab39c88e3d49b6a97495c304459f5e3
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-11-25 22:20:20 -0800

    Add code for spotlight, spacy, imdb annotation

--- redial_annotate.py
+++ redial_annotate.py
@@ -3,14 +3,21 @@ import json
 import logging
 import argparse
 import os
+import spacy
+import imdb
+
+from data_utils import load_conversations, dump_conversations_to_file, popular_actors_list, popular_directors_list
+
+from annotation_utils import DBPedia, process_text
+
+from fuzzywuzzy import fuzz, process
 
 import pdb
-import re
 from tqdm.auto import tqdm
 
 from collections import defaultdict
 
-multi_spaces_pattern = re.compile(r"\s+")
+nlp = spacy.load("en_core_web_lg")
 
 def spotlight_annotate(text):
     """
@@ -35,52 +42,6 @@ def spotlight_annotate(text):
 
 def spotlight_annotate_conversations(conversations):
 
-    # These correspond to labels that have been misrecognized
-    blacklist_URIs = {"http://dbpedia.org/resource/Glossary_of_tennis_terms", 
-    "http://dbpedia.org/resource/Good_Movie",
-    "http://dbpedia.org/resource/Sierra_Entertainment",
-    "http://dbpedia.org/resource/Nice",
-    "http://dbpedia.org/resource/Take_Care_(album)",
-    "http://dbpedia.org/resource/Cloning",
-    "http://dbpedia.org/resource/Blood",
-    "http://dbpedia.org/resource/Downhill_creep",
-    "http://dbpedia.org/resource/Movies",
-    "http://dbpedia.org/resource/Hey_There",
-    "http://dbpedia.org/resource/Swimming_(sport)",
-    "http://dbpedia.org/resource/Princess_Falls",
-    "http://dbpedia.org/resource/Haha_(entertainer)",
-    "http://dbpedia.org/resource/LOL",
-    "http://dbpedia.org/resource/Drag_queen",
-    "http://dbpedia.org/resource/Yea_Football_Club",
-    "http://dbpedia.org/resource/Oh_Yeah_(Yello_song)",
-    "http://dbpedia.org/resource/Scalable_Coherent_Interface",
-    "http://dbpedia.org/resource/CAN_bus",
-    "http://dbpedia.org/resource/The_New_One_(horse)",
-    "http://dbpedia.org/resource/Information_technology",
-    "http://dbpedia.org/resource/The_Glad_Products_Company",
-    "http://dbpedia.org/resource/AM_broadcasting",
-    "http://dbpedia.org/resource/To_Heart",
-    "http://dbpedia.org/resource/National_Organization_for_Women",
-    "http://dbpedia.org/resource/Hit_or_Miss_(New_Found_Glory_song)",
-    "http://dbpedia.org/resource/Canada",
-    "http://dbpedia.org/resource/Different_Things",
-    "http://dbpedia.org/resource/Norwegian_Trekking_Association",
-    "http://dbpedia.org/resource/Take_One_(Canadian_magazine)",
-    "http://dbpedia.org/resource/For_Inspiration_and_Recognition_of_Science_and_Technology",
-    "http://dbpedia.org/resource/Two_Guys",
-    "http://dbpedia.org/resource/The_Sydney_Morning_Herald",
-    "http://dbpedia.org/resource/Booting",
-    "http://dbpedia.org/resource/Precious_Time_(album)",
-    "http://dbpedia.org/resource/I\\u0027m_Glad",
-    "http://dbpedia.org/resource/Social_Democratic_Party_of_Switzerland",
-    "http://dbpedia.org/resource/International_Maritime_Organization",
-    "http://dbpedia.org/resource/LOL",
-    "http://dbpedia.org/resource/Names_of_God_in_Judaism",
-    "http://dbpedia.org/resource/Ike_Turner",
-    "http://dbpedia.org/resource/Tricky_Stewart",
-    "http://dbpedia.org/resource/Movies!",
-    }
-
     categories_to_not_stop_at = {
         "http://dbpedia.org/resource/Romantic_comedy_film",
         "http://dbpedia.org/resource/Cartoon",
@@ -102,10 +63,11 @@ def spotlight_annotate_conversations(conversations):
                 continue
             spotlight_annotations = spotlight_annotate(process_text(message["text"]))
 
-
+            cleaned_mentions = []
+            
             if spotlight_annotations.get("Resources"):
                 for entity in spotlight_annotations["Resources"]:
-                    if entity["@URI"] in blacklist_URIs:
+                    if entity["@URI"] in DBPedia.BLACKLIST_URIS:
                         break
                     if entity["@types"]:
                         for t in entity["@types"].split(","):
@@ -113,29 +75,130 @@ def spotlight_annotate_conversations(conversations):
                 # else:
                     # if entity["@URI"] not in categories_to_not_stop_at:                   
                     #     pdb.set_trace()
-                cleaned_mentions = [entity for entity in spotlight_annotations["Resources"] if entity["@URI"] not in blacklist_URIs]
-                message["spotlight_mentions"] = cleaned_mentions
+                cleaned_mentions = [entity for entity in spotlight_annotations["Resources"] if entity["@URI"] not in DBPedia.BLACKLIST_URIS]
+            
+            message["spotlight_mentions"] = cleaned_mentions
     return conversations
     
 
-def process_text(text):
-    return multi_spaces_pattern.sub(" ", text.capitalize())
 
-def load_conversations(conversations_file):
-    with open(conversations_file, 'r') as conversations_file:
-        conversations = []
 
-        for line in conversations_file:
-            conversations.append(json.loads(line.strip()))
+def spacy_ner_annotate(conversations):
+    recognized_category_types = defaultdict(int)
+
+    for conversation in tqdm(conversations):
+        for message in conversation["messages"]:
+            doc = nlp(process_text(message["text"]))
+
+            message_entities = []
+            for ent in doc.ents:
+                message_entities.append({
+                    "surface": ent.text,
+                    "type": ent.label_ 
+                })
+
+                recognized_category_types[ent.label_] += 1
+            
+            message["spacy_mentions"] = message_entities
     
-        return conversations
+    print(recognized_category_types)
 
-def dump_conversations_to_file(conversations, output_file):
-    with open(output_file, 'w') as conversations_file:
-        for conversation in conversations:
-            conversations_file.write(json.dumps(conversation))
-            conversations_file.write('\n')
+    return conversations
 
+
+def link_mention_to_imdb(conversations, imdb_sqlite_path=None):
+    top_actors = popular_actors_list()
+    top_directors = popular_directors_list()
+
+    if imdb_sqlite_path:
+        ia = imdb.IMDb('s3', os.path.join('sqlite+pysqlite:///', args.imdb_sqlite_path))
+    else:
+        ia = imdb.IMDb()
+
+    match_cache = {}
+
+    for conversation in tqdm(conversations):
+        for message in conversation["messages"]:
+            imdb_records = []
+
+            if "spotlight_mentions" in message:
+
+                for mention in message["spotlight_mentions"]:
+                    if "Schema:Person" in mention["@types"]:
+                        # Actor disambiguation logic
+                        surface_form = mention["@surfaceForm"]
+
+                        if surface_form == "Goodnight":
+                            continue
+                        
+                        if surface_form.lower() == "jim" and "jim carrey" in message["text"].lower():
+                            print("Jim converted to jim carrey")
+                            surface_form = "Jim Carrey"
+
+                        top_director_match, top_director_score = process.extractOne(surface_form, top_directors)
+
+                        if top_director_score == 100:
+                            # Tolerate only exact matches for director
+                            print("Matched director ", top_director_match, "for ", surface_form)
+
+                            
+                            cached_result = match_cache.get(best_match)
+
+                            if cached_result:
+                                imdb_records.append(cached_result)
+                            else:
+                                imdb_person_results = ia.search_person(best_match)
+
+                                if len(imdb_person_results) > 0:
+                                    imdb_records.append(imdb_person_results[0])
+                                    match_cache[best_match] = imdb_person_results[0]
+                        else:
+                            top_actor_matches = process.extract(surface_form, top_actors, limit=5, scorer=fuzz.partial_ratio)
+
+                            num_high_matches = len([match for match, score in top_actor_matches if score > 80])
+
+                            if num_high_matches > 1:
+                                print("Warning! Multiple matches for ", surface_form, " ; Top matches: ", top_actor_matches)
+                                num_100_matches = len([match for match, score in top_actor_matches if score == 100])
+
+                                if num_100_matches == 1:
+                                    
+                                    best_match = top_actor_matches[0][0]
+                                    print("Choosing closest match ", best_match)
+
+                                    cached_result = match_cache.get(best_match)
+
+                                    if cached_result:
+                                        imdb_records.append(cached_result)
+                                    else:
+                                        imdb_person_results = ia.search_person(best_match)
+                                        if len(imdb_person_results) > 0:
+                                            imdb_records.append(imdb_person_results[0])
+                                            match_cache[best_match] = imdb_person_results[0]
+                                else:
+                                    print("Too many high scoring matches!Ignoring!")
+                                    print("Utterance: ", message["text"])
+                                
+                            elif num_high_matches == 0:
+                                print("Warning! No match for surface form ", surface_form)
+                                print("Utterance: ", message["text"])
+
+                            else:
+                                best_match = top_actor_matches[0][0]
+                                cached_result = match_cache.get(best_match)
+
+                                if cached_result:
+                                    imdb_records.append(cached_result)
+                                else:
+                                    imdb_person_results = ia.search_person(best_match)
+                                    if len(imdb_person_results) > 0:
+                                        imdb_records.append(imdb_person_results[0])
+                                        match_cache[best_match] = imdb_person_results[0]
+
+            message["imdb_entries"] = [item.asXML() for item in imdb_records]
+    return conversations
+
+                
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -144,12 +207,18 @@ if __name__ == "__main__":
         type=str,
         default="redial"
     )
+    parser.add_argument(
+        '--imdb_sqlite_path',
+        type=str,
+        default=None,
+        help="Path to the IMDB sqlite database. Optional, but makes search significantly faster"
+    )
 
     args = parser.parse_args()
 
     splits = {
-        "train": {"input_file": "train_data.jsonl", "output_file": "train_data_spotlight.jsonl"},
-        "test": {"input_file": "test_data.jsonl", "output_file": "test_data_spotlight.jsonl"}
+        "train": {"input_file": "train_data_spacy.jsonl", "output_file": "train_data_imdb.jsonl"},
+        "test": {"input_file": "test_data_spacy.jsonl", "output_file": "test_data_imdb.jsonl"}
     }
     
     for split, metadata in splits.items():
@@ -158,7 +227,7 @@ if __name__ == "__main__":
 
         conversations = load_conversations(split_filepath)
 
-        annotated_conversations = spotlight_annotate_conversations(conversations)
+        annotated_conversations = link_mention_to_imdb(conversations, args.imdb_sqlite_path)
 
         out_filepath = os.path.join(args.dataset_path, metadata["output_file"])
         dump_conversations_to_file(annotated_conversations, out_filepath)
