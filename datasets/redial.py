fb0142269402316e9975c2af4d679d33f6ed5ed0
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-12-02 13:45:23 -0800

    Setup TransferTransfo training

--- datasets/redial.py
+++ datasets/redial.py
@@ -1,3 +1,5 @@
+import random
+
 from torch.utils.data import Dataset
 
 from itertools import chain
@@ -65,3 +67,87 @@ class RedialDialoGPTDataset(Dataset):
 
     def __len__(self):
         return len(self.dataset)
+
+
+
+class RedialTransferTransfoDataset(Dataset):
+    """
+    Mimics the setup from the TransferTransfo paper. This setup has been
+    fairly reliable for knowledge grounded models such as Topical Chats.
+    """
+
+    def __init__(self, dataset, tokenizer, special_tokens, args):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.special_tokens = special_tokens
+
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.special_tokens = special_tokens
+
+        self.max_history = args.max_history_turns
+        self.num_candidates = args.num_candidates
+
+    @staticmethod
+    def sample_candidates(dataset, index, num_samples):
+        # Sample candidates IID
+        candidates = [response for (_, response, _) in random.sample(dataset, num_samples)]
+
+        return candidates
+
+    @staticmethod
+    def build_input_from_segments(context, response, knowledge, tokenizer, special_tokens, lm_labels):
+
+        bos, eos, speaker1, speaker2 = tokenizer.convert_tokens_to_ids(special_tokens[:4])
+
+        sequence = [[bos] + knowledge] + context + [response + [eos]]
+        sequence = [sequence[0]] + [
+            [(speaker2 if (len(sequence) - i) % 2 else speaker1)] + s for i, s in enumerate(sequence[1:])]
+
+        instance = {}
+        instance["input_ids"] = list(chain.from_iterable(sequence))
+        instance["token_type_ids"] = [speaker2 if i % 2 else speaker1 for i, s in enumerate(sequence) for _ in s]
+        instance["mc_token_ids"] = len(instance["input_ids"]) - 1
+
+        if lm_labels:
+            instance["lm_labels"] = ([-100] * sum(len(s) for s in sequence[:-1])) + [-100] + sequence[-1][1:]
+        else:
+            instance["lm_labels"] = [-100] * len(instance["input_ids"])
+
+        return instance
+
+    @staticmethod
+    def truncate_inputs(context, max_context_exchanges):
+        # Limit the number of context exchanges to retain
+        if len(context) > (2 * max_context_exchanges + 1):
+            context = context[-(2 * max_context_exchanges + 1):]
+
+        return context
+
+    def __getitem__(self, index):
+
+        context, response, knowledge = self.dataset[index]
+
+        context = self.truncate_inputs(context, self.max_history)
+        tokenized_context = [self.tokenizer.encode(turn) for turn in context]
+        tokenized_response = self.tokenizer.encode(response)
+
+        candidates = self.sample_candidates(self.dataset, index, self.num_candidates - 1)
+
+        tokenized_candidates = [self.tokenizer.encode(candidate) for candidate in candidates]
+        tokenized_candidates.append(tokenized_response)
+
+        instances = []
+
+        for j, candidate in enumerate(tokenized_candidates):
+            lm_labels = bool(j == self.num_candidates - 1)
+            instance = self.build_input_from_segments(tokenized_context, candidate, [], self.tokenizer, self.special_tokens, lm_labels)
+            instances.append(instance)
+
+        return instances
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def get_num_batches(self, batch_size):
+        return len(self) // batch_size # has the effect of performing "drop last"
